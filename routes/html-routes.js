8f2be116a99e4191dcbdfca77ca803e66f528925
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-05 20:18:24 -0500

    Trouble shooting paths

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ var path = require("path");
 
 module.exports = (app) => {    
     // This is the route to go to the notes page
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
     // This is the route to go to the index page
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
